5a4b7294e4d5ce0a03fd682765adf93f2e3ad37e
Jose Luis <joseluis.llorenteperales@gmail.com>
2024-04-03 12:24:59 +0200

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,11 +2,11 @@
 public class HolaMundo {
 
 	public static void main (String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo2");
 		
-		//Comentario de línea
-		int numero = 7;//Declaración e inicialización
-		double numeroDecimal;//Declaración
+		//Comentario de lÃ­nea
+		int numero = 7;//DeclaraciÃ³n e inicializaciÃ³n
+		double numeroDecimal;//DeclaraciÃ³n
 		char caracter = 'a';
 		boolean esPar = true;
 		numeroDecimal = 9.5;
